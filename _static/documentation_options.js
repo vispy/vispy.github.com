3d1d8a122c56d11e55da14431beab498345874f0
David Hoese <david.hoese@ssec.wisc.edu>
2019-11-04 20:39:29 -0600

    Update (automated commit)

--- _static/documentation_options.js
+++ _static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '',
+    VERSION: '0.6.2.dev27+g8b71f938.d20191104',
     LANGUAGE: 'None',
     COLLAPSE_INDEX: false,
     FILE_SUFFIX: '.html',
