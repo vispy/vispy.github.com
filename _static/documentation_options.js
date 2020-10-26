19c9951b6d7661c6da79c5c49153e7688202d542
David Hoese (via Travis CI) <david.hoese@ssec.wisc.edu>
2020-10-26 13:10:42 +0000

    Deploy vispy.org website for SHA:51f102bfb26fe8f6175f1fdf3dd89b3d193d27ae (Tag: )

--- _static/documentation_options.js
+++ _static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '0.6.5.dev511+gb416613',
+    VERSION: '0.6.5.dev554+g51f102b',
     LANGUAGE: 'None',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
