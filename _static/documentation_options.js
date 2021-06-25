9b8f6bd69585f7cec842067b7cb9fa306b123469
almarklein <almarklein@users.noreply.github.com>
2021-06-25 07:58:32 +0000

    Deploy vispy.org website for SHA:184d854c7542f4c6df21bb4c7db3dd5b340dd900 (Ref: refs/heads/main)

--- _static/documentation_options.js
+++ _static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '0.6.5.dev712+ge8d395dd',
+    VERSION: '0.6.5.dev713+g184d854c',
     LANGUAGE: 'None',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
