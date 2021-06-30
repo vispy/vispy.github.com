6f263e82c72978f780c39d7d9c80f4127558e91c
djhoese <djhoese@users.noreply.github.com>
2021-06-30 16:06:19 +0000

    Deploy vispy.org website for SHA:44c22a4aa1f4cf70681d7d608b95f22644d4dcb7 (Ref: refs/heads/main)

--- _static/documentation_options.js
+++ _static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '0.6.5.dev720+g45e365e7',
+    VERSION: '0.6.5.dev721',
     LANGUAGE: 'None',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
