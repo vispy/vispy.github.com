06a0aab9eb5f966098537670aaa4147f78cc1063
brisvag <brisvag@users.noreply.github.com>
2022-11-07 15:09:41 +0000

    Deploy vispy.org website for SHA:df6c6be9c5aa6a67abf7e3072780264886e2be77 (Ref: refs/heads/main)

--- _static/documentation_options.js
+++ _static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '0.11.1.dev137',
+    VERSION: '0.11.1.dev140',
     LANGUAGE: 'en',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
